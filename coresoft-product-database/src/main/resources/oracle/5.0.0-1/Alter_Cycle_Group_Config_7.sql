a5e36463d3dba3c4e6c5a047bb09e605c4ff0b4c
psreedhara <psreedhara@3b2d0187-5808-0410-b659-dd976087eea1>
2017-10-16 12:39:16 +0000

    Initial import.
    git-svn-id: http://svn.vsoft.cloud:8080/svn/coresoft/branches/Coresoft-5.0.0/vsoft-database-setup-lat-parent-latest@34993 3b2d0187-5808-0410-b659-dd976087eea1

--- coresoft-product-database/src/main/resources/oracle/5.0.0-1/Alter_Cycle_Group_Config_7.sql
+++ coresoft-product-database/src/main/resources/oracle/5.0.0-1/Alter_Cycle_Group_Config_7.sql
@@ -4,5 +4,3 @@ alter table automatic_transfer_instruction drop constraint AT_TARGET_ACC;
 alter table automatic_transfer_instruction drop constraint AT_SRC_ACC;
 
 alter table CIF_FIELDS_MASTER ADD(IS_MANDATORY NUMBER(1),FIELD_VIEW VARCHAR2(200));
-
-
