4cef7ae8f12b00f18c704e552d9ffcbd5a0b5ed3
kvaddepalli <kvaddepalli@3b2d0187-5808-0410-b659-dd976087eea1>
2017-12-28 07:17:41 +0000

    removed insert scripts
    git-svn-id: http://svn.vsoft.cloud:8080/svn/coresoft/branches/Coresoft-5.0.0/vsoft-database-setup-lat-parent-latest@36851 3b2d0187-5808-0410-b659-dd976087eea1

--- coresoft-product-database/src/main/resources/oracle/5.0.0-2/How_Did_You_Hear_About_us_7.sql
+++ coresoft-product-database/src/main/resources/oracle/5.0.0-2/How_Did_You_Hear_About_us_7.sql
@@ -6,10 +6,3 @@ CREATE TABLE "HOW_DID_YOU_HEAR_ABT_US"
    CONSTRAINT "HOW_DID_YOU_HEAR_ABT_US_PK" PRIMARY KEY ("UNIQUE_ID") ENABLE);
    
 create sequence HOW_DID_YOU_HEAR_ABT_US_SEQ minvalue 1 maxvalue 9999999999 increment by 1 start with 1;
-
-select * from how_did_you_hear_abt_us;
-Insert into how_did_you_hear_abt_us (UNIQUE_ID,INFORMATION,ENABLE) values (1,'Existing Customer',1);
-Insert into how_did_you_hear_abt_us (UNIQUE_ID,INFORMATION,ENABLE) values (2,'Referred by Customer',1);
-Insert into how_did_you_hear_abt_us (UNIQUE_ID,INFORMATION,ENABLE) values (3,'Referred by Banker',1);
-Insert into how_did_you_hear_abt_us (UNIQUE_ID,INFORMATION,ENABLE) values (4,'Advertising',1);
-Insert into how_did_you_hear_abt_us (UNIQUE_ID,INFORMATION,ENABLE) values (5,'Drive By',1);
