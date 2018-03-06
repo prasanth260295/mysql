f77e3b1e57a7d89437f6871072cc769b0203561e
kvaddepalli <kvaddepalli@3b2d0187-5808-0410-b659-dd976087eea1>
2018-03-06 06:57:02 +0000

    added alter script on package line item table
    git-svn-id: http://svn.vsoft.cloud:8080/svn/coresoft/branches/Coresoft-5.0.0/vsoft-database-setup-lat-parent-latest@39883 3b2d0187-5808-0410-b659-dd976087eea1

---
+++ coresoft-product-database/src/main/resources/oracle/5.0.0-6/Alter_Package_Line_Item_4.sql
@@ -0,0 +1,3 @@
+alter table package_line_item add SAVINGS_TRAN_CODE NUMBER(10);
+alter table package_line_item add CHEKING_TRAN_CODE NUMBER(10);
+alter table package_line_item add SERVICE_REQUEST_ID NUMBER(10);
